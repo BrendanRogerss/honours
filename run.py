b25ad3a66432a5b37889c7aeb5186e4eeb4d3ca9
brendan <brendan.rogerz@gmail.com>
2018-09-27 20:40:19 +1000

    adding log files adding command line argument for run

--- run.py
+++ run.py
@@ -1,8 +1,19 @@
 import train
 import time
-
+import argparse
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', type=int, default=0)
+    parser.add_argument('--buffer', type=int, default=0)
+    args = parser.parse_args()
+
+    buffers = [1000,10000,100000]
+    games = ['VideoPinballNoFrameskip-v4', 'GopherNoFrameskip-v4', 'RoadRunnerNoFrameskip-v4', 'DemonAttackNoFrameskip-v4', 'AssaultNoFrameskip-v4', 'KangarooNoFrameskip-v4','BeamRiderNoFrameskip-v4', 'KungFuMasterNoFrameskip-v4','QbertNoFrameskip-v4']
     start_time = time.time()
-    train.main('AtlantisNoFrameskip-v4',100000)
-    print("--- %s seconds ---" % (time.time() - start_time))
+    g = games[args.env]
+    b = buffers[args.buffer]
+    train.main(g,b)
+    with open('times.txt', 'a') as myfile:
+        line = g+"_"+str(b)+":"+str(time.time() - start_time)+"\n"
+        myfile.write(line)
